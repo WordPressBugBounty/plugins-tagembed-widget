73e03dfaff0e790b717dfdbf8198d41705262eaa
Sean Murphy <sean@iamseanmurphy.com>
2026-03-27 10:29:11 +0000

    7.2

--- tagembed-widget/tagembed.php
+++ tagembed-widget/tagembed.php
@@ -1,19 +1,21 @@
 <?php
 
 /**
- * Plugin Name:       Tagembed Widget
+ * Plugin Name:       Tagembed: Social Feeds Widget
  * Plugin URI:        https://tagembed.com/
- * Description:       Display Facebook feed, Instagram feed, Twitter feed, YouTube Videos and more social feeds from 15+ social networks on any page, posts or widgets using shortcode. Beautifully clean, customizable, and responsive Social Media Feed Widget Plugin for WordPress.
- * Version:           7.1
+ * Description:       Display social media feeds and user-generated content in an interactive widget.
+ * Version:           7.2
  * Author:            Tagembed
  * Author URI:        https://tagembed.com/
+ * License:           GPLv3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  */
 if (!defined('WPINC')) :
 	die;
 endif;
 
 /* --Start-- Create Constant */
-!defined('TAGEMBED_PLUGIN_VERSION')          && define('TAGEMBED_PLUGIN_VERSION', '7.1');
+!defined('TAGEMBED_PLUGIN_VERSION')          && define('TAGEMBED_PLUGIN_VERSION', '7.2');
 !defined('TAGEMBED_PLUGIN_DIR_PATH')         && define('TAGEMBED_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
 !defined('TAGEMBED_PLUGIN_URL')              && define('TAGEMBED_PLUGIN_URL', plugin_dir_url(__FILE__));
 !defined('TAGEMBED_PLUGIN_REDIRECT_URL')     && define('TAGEMBED_PLUGIN_REDIRECT_URL', get_admin_url(null, 'admin.php?page='));
